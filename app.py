377a723f9492d8470ab7b3308f5a46f9158586d4
Antal János <janos.antal@gmail.com>
2024-06-05 10:20:57 +0200

    Changing to FastAPI

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask, request, jsonify
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 from openai import OpenAI
 import prompts as p
 from dotenv import load_dotenv
 import os
-from waitress import serve
 
-app = Flask(__name__)
 load_dotenv()
-# API hívás a "Marketing Mentor" egyéni GPT modellhez
+app = FastAPI()
 
+# API hívás a "Marketing Mentor" egyéni GPT modellhez
 client = OpenAI(api_key=os.getenv("API_KEY"))
 
-def buyer_persona(input_data):
+class InputData(BaseModel):
+    input: str
+
+def buyer_persona(input_data: str):
     response = client.chat.completions.create(
         model="gpt-4o",
         messages=[
@@ -21,18 +25,17 @@ def buyer_persona(input_data):
     )
     return response.choices[0].message.content.strip()
 
-@app.route('/buyer_persona', methods=['GET'])
-def create_buyer_persona():
-    input_data = request.args.get('input')  # Szöveg beolvasása a GET paraméterből
-    if not input_data:
-        return jsonify({"error": "Input data is required"}), 400
+@app.get('/buyer_persona')
+async def create_buyer_persona(input: str):
+    if not input:
+        raise HTTPException(status_code=400, detail="Input data is required")
     
     try:
-        result = buyer_persona(input_data)
-        return jsonify({"buyer_persona": result}), 200
+        result = buyer_persona(input)
+        return JSONResponse(content={"buyer_persona": result}, status_code=200)
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        raise HTTPException(status_code=500, detail=str(e))
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
-serve(app, host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host='0.0.0.0', port=8080)
